6ee84af7ee086aa79df161567b475b70cb87c3ce
Acdumond <email@andrew-dumond.com>
2021-10-28 12:40:17 -0400

    linked the array elements of the api call to the html table

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,5 +1,9 @@
 var userTickerInput = document.querySelector("#userInputTicker");
 var submitButton = document.querySelector("#submitButton");
+var mCap = document.querySelector("#MarketCap");
+var ticker = document.querySelector("#ticker");
+var linkedCompany = document.querySelector("#inputCompany");
+var companyname = document.querySelector("#company");
 
 function searchTicker() {
   console.log(userTickerInput.value);
@@ -16,8 +20,16 @@ function searchTicker() {
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
+    .then(function show(data) {
       console.log(data);
+      mCap.textContent = data[0].marketCap;
+      ticker.textContent = data[0].symbol;
+      companyname.textContent = data[0].name;
+      linkedCompany.setAttribute(
+        "href",
+        `https://finance.yahoo.com/quote/${data[0].symbol}`
+      );
     });
 }
+
 submitButton.addEventListener("click", searchTicker);
