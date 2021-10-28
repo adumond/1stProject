3ba5522b88050246cae8f612b6e6345c1e583125
Acdumond <email@andrew-dumond.com>
2021-10-28 13:33:48 -0400

    added a new api for the financial ratios

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -4,6 +4,17 @@ var mCap = document.querySelector("#MarketCap");
 var ticker = document.querySelector("#ticker");
 var linkedCompany = document.querySelector("#inputCompany");
 var companyname = document.querySelector("#company");
+var pe = document.querySelector("#PE");
+var peg = document.querySelector("#PEG");
+var cash = document.querySelector("#Cash");
+var debtEquity = document.querySelector("#DebtEquity");
+var current = document.querySelector("#Current");
+var payout = document.querySelector("#Payout");
+
+function buttonClick() {
+  searchTicker();
+  ratioapi();
+}
 
 function searchTicker() {
   console.log(userTickerInput.value);
@@ -25,11 +36,35 @@ function searchTicker() {
       mCap.textContent = data[0].marketCap;
       ticker.textContent = data[0].symbol;
       companyname.textContent = data[0].name;
+      pe.textContent = data[0].pe;
       linkedCompany.setAttribute(
         "href",
         `https://finance.yahoo.com/quote/${data[0].symbol}`
       );
     });
 }
+function ratioapi() {
+  fetch(
+    "https://financialmodelingprep.com/api/v3/ratios/" +
+      userTickerInput.value +
+      "?apikey=2d1a1c7c741d940e02d0caa1d05e8726",
+    {
+      method: "GET", //GET is the default.
+      credentials: "same-origin", // include, *same-origin, omit
+      redirect: "follow", // manual, *follow, error
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function show(data) {
+      console.log(data);
+      peg.textContent = data[0].priceEarningsToGrowthRatio;
+      cash.textContent = data[0].cashRatio;
+      debtEquity.textContent = data[0].debtEquityRatio;
+      current.textContent = data[0].currentRatio;
+      payout.textContent = data[0].payoutRatio;
+    });
+}
 
-submitButton.addEventListener("click", searchTicker);
+submitButton.addEventListener("click", buttonClick);
